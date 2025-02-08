4e3db23e4958808bb38e12ad6eae328a4da59581
Ranjith-Mohan17 <ranjithmohan297@gmail.com>
2025-02-08 11:59:48 +0530

    Third

--- day4/PronicNumber.java
+++ day4/PronicNumber.java
@@ -5,7 +5,7 @@ public class PronicNumber {
 	public static void main(String args[]) {
 	    Scanner sc = new Scanner(System.in);
 	    int n = sc.nextInt();
-	    if(n % 2 == 0) {
+	    if(n % 2 != 0) {
 	    	System.out.println("Not Pronic");
 	    	System.exit(0);
 	    }
